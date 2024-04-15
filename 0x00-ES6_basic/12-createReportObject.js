97ed694ad7b5d109185c5965dc0591b83ba46eca
laila22haz <hazmirlaila316@gmail.com>
2024-04-15 22:04:25 +0100

    Task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,1 +1,8 @@
-
+export default function createReportObject(employeesList) {
+    return {
+        allEmployees: employeesList,
+        getNumberOfDepartments: () => Object.keys(employeesList).length,
+    };
+    //console.log(allEmployees);
+    //console.log(employeesList);
+}
