d09c9bedd76686ffadbd0120e47af54772cc5ac1
laila22haz <hazmirlaila316@gmail.com>
2024-04-17 19:40:21 +0100

    Task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,3 @@
-export default (success) => success ? Promise.resolve({status: 200, body: 'Success'}) : Promise.reject(new Error('The fake API is noworking currently'));
+export default (success) => 
+success ? Promise.resolve({status: 200, body: 'Success'}) : 
+Promise.reject(new Error('The fake API is noworking currently'));
