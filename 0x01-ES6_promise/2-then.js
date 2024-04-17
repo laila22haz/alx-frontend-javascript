97f85c4e8390957317021af829120a89c5b4c762
laila22haz <hazmirlaila316@gmail.com>
2024-04-17 22:42:37 +0100

    Task 1

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,27 @@
+/*
+export default async (promise) => {
+    try {
+        await promise;
+        return {status: 200, body: 'success'};
+    } catch (error) {
+        return new Error();
+    }
+    finally {
+        console.log('Got a response from the API');
+    }
+}
+*/
 
+export default (promise) => {
+    return promise
+        .then(() => {
+            return {status: 200, body: 'success'}
+        }
+        )
+        .catch (() => {
+            return new Error();
+        })
+        .finally (() => {
+            console.log('Got a response from the API');
+        })
+}
