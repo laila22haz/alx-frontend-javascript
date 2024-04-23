e44b5c6d5943e270e1f610a902b472679e3a3f56
laila22haz <hazmirlaila316@gmail.com>
2024-04-23 21:42:00 +0100

    Task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
 
+export default function handleProfileSignup() {
+    const upload = uploadPhoto();
+    const create = createUser();
+    Promise.all([upload, create])
+    .then((res) => {
+        console.log(res.map(item => {
+            return item.body || [item.firstName, item.lastName].join(' ');
+        }).join(' '));
+    })
+    .catch((err) => {
+        console.log('Signup system offline')}
+    );
+}
