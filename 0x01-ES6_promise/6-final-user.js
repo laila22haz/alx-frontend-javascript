ebfee458b511d11f21f5c44ee91acb01984326aa
laila22haz <hazmirlaila316@gmail.com>
2024-05-05 23:07:05 +0100

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
+const handleProfileSignup = async (firstName, lastName, fileName) => {
+    const returnPromises = await Promise.allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(fileName)
+    ]);
+    return returnPromises.map((item) => {
+        return {
+            status: item.status,
+            value: item.value || item.reason
+        }
+    }) 
+}
+
+export default handleProfileSignup;
