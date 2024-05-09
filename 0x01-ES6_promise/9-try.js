12431189d725faa39dac7414baccd244e24aff9a
laila22haz <hazmirlaila316@gmail.com>
2024-05-09 21:58:26 +0100

    Task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,11 @@
-
+export default function guardrail(mathFunction) {
+    const queue = [];
+    try {
+        const execute_math = mathFunction();
+        queue.push(execute_math);
+    } catch(error) {
+        queue.push(`Error: ${error.message}`);
+    }
+    queue.push('Guardrail was processed');
+    return queue
+}
