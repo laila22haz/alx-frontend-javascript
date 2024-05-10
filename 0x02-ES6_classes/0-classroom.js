f6633a3b7fa661d3263a207675011e438ea58096
laila22haz <hazmirlaila316@gmail.com>
2024-05-10 22:50:36 +0100

    First Try

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,17 @@
+
+export default class ClassRoom {
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;
+    }
+}
+
+
+
+
+
+
+/**
+ const room1 = new ClassRoom(10); 
+ const room2 = new ClassRoom(30); 
+ const room3 = new ClassRoom(1550); 
+*/
