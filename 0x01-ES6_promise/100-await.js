10458b5350f11a8a56a824eeb6eb9de3f5cb2a76
laila22haz <hazmirlaila316@gmail.com>
2024-05-10 20:09:23 +0100

    Task 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,18 @@
+import {uploadPhoto, createUser} from './utils.js' ;
+
+export default async function asyncUploadUser() {
+    try {
+        const promise_array = await Promise.all([uploadPhoto(), createUser()]);
+        return {
+            photo: promise_array[0],
+            user: promise_array[1],
+        }
+        
+    } catch (error) {
+        return {
+            photo: null,
+            user: null,
+        }
+    }
+}
 
