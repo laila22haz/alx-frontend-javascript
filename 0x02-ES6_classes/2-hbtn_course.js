904f98749e225351c272973304ecfd2ba3f238ce
laila22haz <hazmirlaila316@gmail.com>
2024-05-11 00:00:54 +0100

    Task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,1 +1,35 @@
-#!/usr/bin/node
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this.name = name;
+        this.length = length;
+        this.students = students;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        if (typeof value === 'string') this._name = value;
+        else throw new TypeError('Name must be a string');
+    }
+
+    get length() {
+        return this._lenght;
+    }
+
+    set length(value) {
+        if (typeof value === 'number') this._length = value;
+        else throw new TypeError('Length must be a number');
+
+    }
+
+    get students() {
+        return this._students;
+    }
+
+    set students(value) {
+        if (Array.isArray(value) && value.every(element => typeof element === 'string')) this._students = value;
+        else throw new TypeError('Students must be a array of strings');
+    }
+}
