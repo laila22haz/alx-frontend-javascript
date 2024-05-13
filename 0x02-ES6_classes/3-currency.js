ff00ae91e5904d0ff2fa7805bca089b9760d197c
laila22haz <hazmirlaila316@gmail.com>
2024-05-13 19:36:03 +0100

    Task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,28 @@
-#!/usr/bin/node
+export default class Currency {
+    constructor(code, name) {
+        this.name = name;
+        this.code = code;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        if (typeof value === 'string') this._name = value;
+        else throw new TypeError('Name must be a string');
+    }
+    
+    get code() {
+        return this._code;
+    }
+    
+    set code(value) {
+        if (typeof value === 'string') this._code = value;
+        else throw new TypeError('Code must be a string');
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
+}
