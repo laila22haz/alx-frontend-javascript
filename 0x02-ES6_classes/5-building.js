d689fa33179be7215858edb1c18619acf2188479
laila22haz <hazmirlaila316@gmail.com>
2024-05-14 21:38:34 +0100

    Task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+export default class Building {
+    constructor(sqft) {
+        this.sqft = sqft;
+        if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+
+    set sqft(value) {
+        if (typeof value === 'number') this._sqft = value;
+        else throw new TypeError('Sqft must be a number');
+    }
+}
