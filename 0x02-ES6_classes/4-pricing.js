f3748621f23404aca4c08fc7fa42eadc11b291b8
laila22haz <hazmirlaila316@gmail.com>
2024-05-14 20:53:11 +0100

    Task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,34 @@
-#!/usr/bin/node
+import Currency from "./3-currency";
+
+export default class Pricing {
+    constructor(amount , currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    set amount(value) {
+        if (typeof value === 'number') this._amount = value;
+        else throw new TypeError('Amount must be a number');
+    }
+
+    get currency() {
+        return this._code;
+    }
+    
+    set currency(value) {
+        if (value instanceof Currency) this._currency = value;
+        else throw new TypeError('Currency must be a currency');
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.displayFullCurrency()}`
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
