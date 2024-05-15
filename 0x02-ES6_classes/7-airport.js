35027ab63cf8d07ce8d5acb4a12dadf53ecb11e2
laila22haz <hazmirlaila316@gmail.com>
2024-05-15 21:26:20 +0100

    Task 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,28 @@
-#!/usr/bin/node
+export default class Airport {
+    constructor(name, code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        if (typeof value === 'string') this._name = value;
+        else throw new TypeError('Name must be a string');
+    }
+    
+    get code() {
+        return this._code;
+    }
+    
+    set code(value) {
+        if (typeof value === 'string') this._code = value;
+        else throw new TypeError('Code must be a string');
+    }
+
+    toString() {
+        return `[object ${this._code}]`
+    }
+}
