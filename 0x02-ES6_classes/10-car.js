e269b17c52abdef47e28c294b3d5016252910d22
laila22haz <hazmirlaila316@gmail.com>
2024-05-15 23:56:11 +0100

    Task 100

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -2,37 +2,9 @@ const symbolES = Symbol('cloneCar');
 
 export default class Car {
     constructor(brand, motor, color) {
-        this.brand = brand;
-        this.motor = motor;
-        this.color = color;
-    }
-
-    get brand() {
-        return this._brand;
-    }
-
-    set brand(value) {
-        if (typeof value === 'string') this._brand = value;
-        else throw new TypeError('brand must be a string');
-    }
-    
-    get motor() {
-        return this._motor;
-    }
-    
-    set motor(value) {
-        if (typeof value === 'string') this._motor = value;
-        else throw new TypeError('motor must be a string');
-    }
-
-
-    get color() {
-        return this._motor;
-    }
-    
-    set color(value) {
-        if (typeof value === 'string') this._color = value;
-        else throw new TypeError('color must be a string');
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
     }
 
     [symbolES]() {
