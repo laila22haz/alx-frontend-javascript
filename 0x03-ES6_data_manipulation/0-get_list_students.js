a270002b852cd2757bf6aa388de519015316976f
laila22haz <hazmirlaila316@gmail.com>
2024-05-16 21:50:43 +0100

    Task 1

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -10,7 +10,7 @@ export default () => [
             location: 'Columbia',
         },
         {
-            id: 3, 
+            id: 5, 
             firstName: 'Serena',
             location: 'San Francisco',
         }
