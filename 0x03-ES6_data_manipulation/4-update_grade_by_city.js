c763c5ed9b965a24a7062e2178214bde31355d70
laila22haz <hazmirlaila316@gmail.com>
2024-05-17 23:15:47 +0100

    Task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default (listStudent, city, newGrades) => {
+    const list = listStudent.filter(Element => Element.location===city);
+    return list.map(element => {
+        element.grade = 'N/A';
+        newGrades.map(el => {
+            if (el.studentId === element.id) element.grade = el.grade;
+        })
+        return element;
+    })
+}
