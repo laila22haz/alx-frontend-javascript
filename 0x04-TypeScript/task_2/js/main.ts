7085010b64163ea647465b6fbbf8514ddc6176f6
Laila_Hazmir <125223268+laila22haz@users.noreply.github.com>
2024-05-30 22:03:43 +0000

    Task 7

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -46,6 +46,31 @@ const createEmployee = (salary: string | number): Director | Teacher => {
 
 }
 
+const isDirector = (employee: Director | Teacher): boolean => 
+{
+    if (employee instanceof Director) return true;
+    return false;
+}
+
+const executeWork = (employee: Director | Teacher) => {
+  if (isDirector(employee)) return new Director().workDirectorTasks();
+  return new Teacher().workTeacherTasks();
+}
+
+type Subjects = 'Math' | 'History';
+const teachClass = (todayClass: Subjects): string => `Teaching ${todayClass}`;
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
+
+
+console.log(executeWork(createEmployee(254)));
+console.log(executeWork(createEmployee(7894668)));
+
+
+
+
+
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
